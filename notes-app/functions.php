89761c40c31c4d626dcd1abd9ddfa1003fb1ef05
Luca Di Leo <lucadileo70@gmail.com>
2026-03-20 10:57:43 +0100

    refactor: move functions

--- notes-app/functions.php
+++ notes-app/functions.php
@@ -7,3 +7,21 @@ function dd($value) {
     echo "</pre>";
     die();
 }
+
+// ---- Router Function ----
+function resolve_route($routes) {
+    $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+    // Se la route esiste, torna il file
+    if (array_key_exists($uri, $routes)) {
+        return $routes[$uri];
+    }
+
+    // Altrimenti ritorna 404
+    return abort(404);
+}
+
+// ---- 404 Function ----
+function abort($code) {
+    http_response_code($code);
+    return "controllers/{$code}.php";
+}
--- notes-app/router.php
+++ notes-app/router.php
@@ -10,23 +10,7 @@ $routes = [
 ];
 
 // dd(parse_url($_SERVER['REQUEST_URI'])['path']);
-// ---- Router Function ----
-function resolve_route($routes) {
-    $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-    // Se la route esiste, torna il file
-    if (array_key_exists($uri, $routes)) {
-        return $routes[$uri];
-    }
 
-    // Altrimenti ritorna 404
-    return abort(404);
-}
-
-// ---- 404 Function ----
-function abort($code) {
-    http_response_code($code);
-    return "controllers/{$code}.php";
-}
 
 // ---- Include la pagina corretta ----
 $page = resolve_route($routes);

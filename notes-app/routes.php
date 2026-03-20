1114f65164c7bd42335712d94de59ebfcd63fd17
Luca Di Leo <lucadileo70@gmail.com>
2026-03-20 12:14:58 +0100

    fix: minor bug

--- notes-app/routes.php
+++ notes-app/routes.php
@@ -5,6 +5,6 @@ return [
     "/about"     => "controllers/about.php",
     "/contact"   => "controllers/contact.php",
     "/notes"     => "controllers/notes/index.php",
-    "/notes/show"     => "controllers/notes/show.php",
+    "/note"     => "controllers/notes/show.php",
     "/notes/create"     => "controllers/notes/create.php",
 ];

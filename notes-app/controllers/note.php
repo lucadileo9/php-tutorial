3ed16d6f1b9a336eb52e4a0a1c1d658ff460b997
Luca Di Leo <lucadileo70@gmail.com>
2026-03-20 11:04:34 +0100

    feat: add weak authorization

--- notes-app/controllers/note.php
+++ notes-app/controllers/note.php
@@ -6,4 +6,12 @@ $db = new Database($config["database"], "root", "");
 
 $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
 
+if (!$note) {
+    abort(404);
+}
+
+if ($note['user_id'] != 1) {
+    abort(403);
+}
+
 require 'views/note.view.php';
